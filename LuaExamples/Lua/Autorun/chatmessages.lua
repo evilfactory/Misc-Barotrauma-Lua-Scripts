9eb36cf1b09001277b433a45f26f9e88d61b8f2a
EvilFactory <evilfactory@users.noreply.github.com>
2024-06-13 21:35:44 -0300

    Clarification in chatmessage

--- LuaExamples/Lua/Autorun/chatmessages.lua
+++ LuaExamples/Lua/Autorun/chatmessages.lua
@@ -10,6 +10,7 @@ Hook.Add("chatMessage", "examples.chatMessages", function (message, client)
     if message ~= "!announce" then return end
 
     for key, client in pairs(Client.ClientList) do
+        -- Note: this ChatMessage cannot be reused multiple times, you need to create a new one for each new message to each client
         local chatMessage = ChatMessage.Create("John", "Hi Everyone!", ChatMessageType.Default, nil, nil)
         chatMessage.Color = Color(255, 255, 0, 255)
         Game.SendDirectChatMessage(chatMessage, client)
